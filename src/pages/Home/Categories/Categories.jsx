82ee9f4d6947aea7a13c096e6f0a050c2aa0a483
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-05-10 10:06:42 +0600

    added categories section

---
+++ src/pages/Home/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+
+const Categories = () => {
+    return (
+        <section className="max-w-screen-lg mx-auto my-20">
+            <div className="text-center">
+                <h2 className="text-2xl text-accent font-semibold">Job Category List</h2>
+                <p className="text-neutral text-sm mt-2">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div></div>
+        </section>
+    );
+};
+
+export default Categories;
